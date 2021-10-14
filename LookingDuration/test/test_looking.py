38f3dae52a030d08a4dbf6dbae57569bdfb5b084
chrisw-wsu <chris.wang@westernsydney.edu.au>
2021-10-14 16:27:38 +1100

    test

---
+++ LookingDuration/test/test_looking.py
@@ -0,0 +1,33 @@
+from LookingDuration.looking import convertTime, get_data, findLookingDuration
+
+import pandas as pd
+import numpy as np
+import time
+import datetime
+import csv
+import copy
+import pytest
+
+PATH = "LookingDuration/test/test_data"
+
+videoData = PATH + "/AD6BMY_JC.csv"
+respData = PATH + "/2021-10-07-21-23-AML.csv"
+
+
+def test_convertTime():
+    with pytest.raises(Exception):
+        newTimeFormat = ld.convertTime("00:00:12:345")
+
+def test_get_data():
+    
+    fromVideo = get_data(videoData)
+    assert(isinstance(videoData, pd.DataFrame))
+
+def test_findLookingDuration():
+    fromVideo = get_data(videoData)
+    fromResp = get_data(respData)
+    i=2 # trial 5 from resp
+    duration = ld.findLookingDuration(fromResp.Start[i], fromResp.End[i], fromVideo.InTime, fromVideo.OutTime, save_duration=False, savetoFile='test.csv').get('Duration')
+    assert(duration >=0)
+
+
